0f27367a6db841127d7a60d0e60a2e19b4287030
leonhsieh-99 <leonhsieh@yahoo.com>
2021-04-18 04:40:34 -0700

    added firebase stuff

---
+++ Database/db_helper.py
@@ -0,0 +1,16 @@
+import pyrebase
+
+class db:
+	def __init__(self):
+		config = {
+		"apiKey": "AIzaSyDe2CZpsYPIMQneper6tEe_qaAO9XDWNzQ",
+		"authDomain": "hive-f0de6.firebaseapp.com",
+		"databaseURL": "https://hive-f0de6.firebaseio.com",
+		"storageBucket": "hive-f0de6.appspot.com",
+		"serviceAccount": "C:\\Users\\14088\\Downloads\\hive-f0de6-firebase-adminsdk-5k61d-4cb9e04361.json"
+		}
+		self.firebase = pyrebase.initialize_app(config)
+		self.auth = self.firebase.auth()
+
+	def add_user(self, email, password):
+		self.auth.create_user_with_email_and_password(email, password)
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from controllers import loginPage
 from controllers import signUpPage
+from Database import db_helper
+
 import streamlit as st
 
-PAGES = {
-	"loginPage": loginPage,
-	"signUpPage": signUpPage
-}
+def main():
+	PAGES = {
+		"loginPage": loginPage,
+		"signUpPage": signUpPage
+	}
 
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+	#hive_db = db_helper.db()
+	#hive_db.add_user("leonhsieh@yahoo.com", "100999shieh")
+
+	st.sidebar.title('Navigation')
+	selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+	page = PAGES[selection]
+	page.app()
+
+if __name__ == "__main__":
+	main()
